39149ee1f667e256c02466105d31a3b82b2beadb
Jeet Sogarwal <jeetsingh@Jeets-MacBook-Air.local>
2023-04-04 00:34:44 +0530

    my first python program

---
+++ airthmetic_operation.py
@@ -0,0 +1,30 @@
+# Write a program to enter two numbers and perform all arithmetic operations
+
+#print("Name:- Jeet Singh ")
+#print("UID:- 21BCS7299")
+
+#a = int(input("Enter your First number = "))
+#b = int(input("Enter your second number = "))
+
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(a/b)
+
+
+print("Name:- Jeet Singh ")
+print("UID:- 21BCS7299")
+
+a = int(input("Enter your First marks = "))
+b = int(input("Enter your second marks = "))
+c = int(input("Enter your 3rd marks = "))
+d = int(input("Enter your 4th marks = "))
+e = int(input("Enter your 5th marks = "))
+
+total=a+b+c+d+e
+print("total Marks: ",total)
+print("Average: ",total/5)
+print("Percentage: ",total/500*100)
+
+
+
